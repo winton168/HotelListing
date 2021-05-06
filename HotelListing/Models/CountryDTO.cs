dbcecd39dcc680be5b682862f3b78435cc69c17a
Winton Wang <winton168@hotmail.com>
2021-05-05 21:04:50 -0400

    Add Database, AutoMapper, Unit of Work and DTOS

---
+++ HotelListing/Models/CountryDTO.cs
@@ -0,0 +1,34 @@
+﻿using HotelListing.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class CreateCountryDTO
+    {
+       
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short country name is too long")]
+        public string shortName { get; set; }
+
+
+    }
+
+
+
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+
+        public virtual IList<HotelDTO> Hotels { get; set; }
+
+    }
+
+}
